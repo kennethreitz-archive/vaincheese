5367ecdd1ed6fdc2e5557751a22ac5f06841f050
Kenneth Reitz <me@kennethreitz.com>
2011-10-28 01:20:19 -0400

    pypi namespace

--- vaincheese.py
+++ vaincheese.py
@@ -4,7 +4,7 @@ import os
 import json
 from urlparse import urlparse
 
-from flask import Flask
+from flask import Flask, jsonify
 from flaskext.cache import Cache
 
 from vanity import downloads_total
@@ -12,6 +12,7 @@ from vanity import downloads_total
 
 app = Flask(__name__)
 
+# Support Heroku's Redis environment.
 p = urlparse(os.environ.get('REDISTOGO_URL', ''))
 
 app.config['CACHE_TYPE'] = 'redis'
@@ -19,27 +20,20 @@ app.config['CACHE_REDIS_HOST'] =  p.hostname or 'localhost'
 app.config['CACHE_REDIS_PORT'] = p.port or 6379
 app.config['CACHE_REDIS_PASSWORD'] = p.password or None
 
-app.debug = True
-
 cache = Cache(app)
-# r = cache.cache._client.connection_pool
-# r.password = p.password
-
-# Support Heroku's Redis environment.
-
 
 
 @app.route('/')
 def index():
     d = {
         'routes': {
-            '/:package': 'Given package\'s download stats.'
+            '/pypi/:package': 'Given package\'s download stats.'
         }
     }
-    return json.dumps(d)
+    return jsonify(d)
 
 
-@app.route('/<package>')
+@app.route('/pypi/<package>')
 @cache.memoize(timeout=6*60*60)
 def package_stats(package):
 
@@ -48,7 +42,7 @@ def package_stats(package):
         'downloads': downloads_total(package)
     }
 
-    return json.dumps(d)
+    return jsonify(d)
 
 
 if __name__ == '__main__':
